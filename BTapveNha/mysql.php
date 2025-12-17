c40d5a0e4883f46ab4b2a16346b42eb58b3fa6ce
nguyentrankg05 <nguyentrankg05@gmail.com>
2025-12-17 20:47:54 +0700

    upload

--- BTapveNha/mysql.php
+++ BTapveNha/mysql.php
@@ -12,10 +12,40 @@ $connection = new mysqli($host, $username, $pwd, $database);
 $sql = "SELECT * From san_pham";
 $data = $connection->execute_query($sql)->fetch_all(MYSQLI_ASSOC);
 
+
 //Đóng kết nối
 $connection->close();
 
 echo "<pre>";
-var_dump($data);
 echo "</pre>";
 
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Danh Sách Sản Phẩm</title>
+    </head>
+    <body>
+        
+        <h2>Hiển Thị Danh Sách Sản Phẩm</h2>
+        
+    <table border="5" cellpadding="5" >
+        <tr>
+            <th>Tên Sản Phẩm:</th>
+            <th>Giá</th>
+            <th>Mô Tả</th>
+        </tr>
+
+        <?php 
+        foreach($data as $sanpham){
+            echo "<tr>";
+            echo "<td>{$sanpham['ten_san_pham']}</td>";
+            echo "<td>{$sanpham['gia']}</td>";
+            echo "<td>{$sanpham['mo_ta']}</td>";
+            echo "</tr>";
+        }
+        ?>
+</table>
+</body>
+</html>
